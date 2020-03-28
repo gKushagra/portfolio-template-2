4df683ede1861fbf6d704e9c573e9885fecb4b32
Kushagra Gupta <kushagragupta@hotmail.co.in>
2020-03-28 18:25:14 -0500

    fetched data from json and displayed on homepage.

--- routes/contact.js
+++ routes/contact.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 module.exports = () => {
   router.get('/', (req, res) => {
-    res.render('contact', { pageTitle: 'Contact' });
+    res.render('layouts/layout', { pageTitle: 'Contact', template: '../contact' });
   });
   return router;
 };
--- routes/home.js
+++ routes/home.js
@@ -4,14 +4,28 @@ const contactRoute = require('./contact');
 
 const router = express.Router();
 
-module.exports = () => {
-    router.get('/', (req, res) => {
-        res.render('home', { pageTitle: 'Profile' });
-    });
+module.exports = (params) => {
+  const { profileDataService } = params;
+  let { cumulativeVisit } = params;
 
-    router.use('/contact', contactRoute());
+  router.get('/', async (req, res) => {
+    if (!req.session.visitcount) {
+      req.session.visitcount = 0;
+    }
 
-    return router;
-}
+    req.session.visitcount += 1;
+    cumulativeVisit += 1;
 
+    // eslint-disable-next-line no-console
+    // console.log(`Number of visits: ${req.session.visitcount}, Cumulative Visits: ${cumulativeVisit}`);
 
+    const jsonData = await profileDataService.getProfileData();
+    return res.render('layouts/layout', { pageTitle: 'Home', template: '../home' });
+    // return res.json(jsonData);
+    // res.render('home', { pageTitle: 'Profile' });
+  });
+
+  router.use('/contact', contactRoute());
+
+  return router;
+};
